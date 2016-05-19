879a03e7f8bf1b8d2b07c5b14fa6e6ffd52feafb
dhcrain <dhcrain@gmail.com>
2016-05-19 14:15:29 -0400

    Opened words, readlines, get random word

--- mystery_word.py
+++ mystery_word.py
@@ -1,1 +1,24 @@
-# imposrt word list
+import my_functions
+import random
+
+with open('/usr/share/dict/words') as opened_file:
+    word_list = opened_file.readlines()
+
+word = random.choice(word_list)
+print("word: " + word)
+guess_max = 8 + len(word)
+guess_count = 0
+guesses = []
+letter = input("Guess a letter: ")
+print(" _" * len(word))
+
+if letter in word:
+
+
+# print instructions
+# tell user how many letters are in the word
+# Let the user know if their guess appears in the computer's word.
+# Display the partially guessed word
+# allow 8 guesses, only loose guess when there is a miss
+# if user runs out of guesses show the word
+# if guess same letter tell them
