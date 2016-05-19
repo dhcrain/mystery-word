4e2dff1e73fcc655bcba486da1e0bae4987ead9e
dhcrain <dhcrain@gmail.com>
2016-05-19 17:33:19 -0400

    Normal mode almost complete, just missing win if double letters in word.

--- mystery_word.py
+++ mystery_word.py
@@ -4,6 +4,7 @@ import random
 with open('/usr/share/dict/words') as opened_file:
     word_list = opened_file.readlines()
 
+# print instructions
 word = random.choice(word_list).upper()
 word = word.replace("\n", "")
 word = list(word)
@@ -13,23 +14,27 @@ bad_guesses = []
 good_guesses = []
 
 while guess_count <= guess_max:
-    print(word)
+    print("".join(word) + ":remove after testing")
 
     for letter in word:
         if letter in good_guesses:
             print(letter, end=" ")
         else:
             print("_", end=" ")
+        if letter not in word:
+            print("win")
+            break
 
+    # if len(good_guesses) == len(word):
+    #     print("You win!")
+    #     break
 
     print("\nBad Guesses [{}/{}]".format(guess_count, guess_max))
     for letter in bad_guesses:
         if letter in bad_guesses:
-            print (letter, end=" ")
-        else:
-            print("_", end=" ")
+            print(letter, end=" ")
 
-    letter = input("\nGuess a letter: ").upper()
+    letter = input("\n" + "_" * 40 + "\nGuess a letter: ").upper()
     if len(letter) != 1:
         print("You can only guess one letter at a time!\n")
     elif letter in bad_guesses or letter in good_guesses:
@@ -45,19 +50,6 @@ while guess_count <= guess_max:
             bad_guesses.append(letter)
             guess_count += 1
         if guess_count == guess_max:
-            print("You Loose! The mystery word was {}".format(str(word)))
+            print("You Loose! The mystery word was {}".format(("".join(word))))
             break
-    if len(good_guesses) == len(word):
-        print("You win!")
-        break
 
-
-
-
-# print instructions
-# tell user how many letters are in the word
-# Let the user know if their guess appears in the computer's word.
-# Display the partially guessed word
-# allow 8 guesses, only loose guess when there is a miss
-# if user runs out of guesses show the word
-# if guess same letter tell them
