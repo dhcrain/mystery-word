bec6fd2cba4620b32b1ca6e5dafd18e119512486
dhcrain <dhcrain@gmail.com>
2016-05-20 11:16:20 -0400

    added option to play the game again.

--- mystery_word.py
+++ mystery_word.py
@@ -5,58 +5,66 @@ import sys
 with open('/usr/share/dict/words') as opened_file:
     word_list = opened_file.readlines()
 
-# print instructions
-word = random.choice(word_list).upper()
-word = word.replace("\n", "")
-word = list(word)
-guess_max = 8
-guess_count = 0
-bad_guesses = []
-good_guesses = []
 
-def display_word():
-    win_t = 0
-    for letter in word:
-        if letter in good_guesses:
-            print(letter, end=" ")
+def game():
+
+    def display_word():
+        win_t = 0
+        for letter in word:
+            if letter in good_guesses:
+                print(letter, end=" ")
+            else:
+                print("_", end=" ")
+                win_t += 1
+        if win_t == 0:
+            print("\n***** Winner *****")
+            play_again()
+
+    word = random.choice(word_list).upper()
+    word = word.replace("\n", "")
+    word = list(word)
+    guess_max = 8
+    bad_guesses = []
+    good_guesses = []
+    guess_count = 0
+    print("Welcome to Mystery Word!\n"
+          "You task is to guess the word in the blanks ({} letters long)\n".format(len(word)))
+    while guess_count <= guess_max:
+        print("".join(word) + ":remove after testing")
+
+        display_word()
+
+        print("\nBad Guesses [{}/{}]".format(guess_count, guess_max))
+        for letter in bad_guesses:
+            if letter in bad_guesses:
+                print(letter, end=" ")
+
+        letter = input("\n" + "_" * 40 + "\nGuess a letter: ").upper()
+
+        if len(letter) != 1:
+            print("You can only guess one letter at a time!\n")
+        elif letter in bad_guesses or letter in good_guesses:
+            print("You already guessed that one!\n")
+        elif not letter.isalpha():
+            print("That's not a letter!\n")
         else:
-            print("_", end=" ")
-            win_t += 1
-    if win_t == 0:
-        print("\n***** Winner *****")
+            if letter in word:
+                print("Good Guess!\n")
+                good_guesses.append(letter)
+            else:
+                print("Bad Guess!\n")
+                bad_guesses.append(letter)
+                guess_count += 1
+
+            if guess_count == guess_max:
+                print("\nYou Loose! The mystery word was {}".format(("".join(word))))
+                play_again()
+
+
+def play_again():
+    if input("Do you want to play again? Y/n ").lower() != "n":
+        game()
+    else:
         sys.exit()
 
-
-while guess_count <= guess_max:
-    print("".join(word) + ":remove after testing")
-
-    display_word()
-
-    print("\nBad Guesses [{}/{}]".format(guess_count, guess_max))
-    for letter in bad_guesses:
-        if letter in bad_guesses:
-            print(letter, end=" ")
-
-    letter = input("\n" + "_" * 40 + "\nGuess a letter: ").upper()
-
-    if len(letter) != 1:
-        print("You can only guess one letter at a time!\n")
-    elif letter in bad_guesses or letter in good_guesses:
-        print("You already guessed that one!\n")
-    elif not letter.isalpha():
-        print("That's not a letter!\n")
-    else:
-        if letter in word:
-            print("Good Guess!\n")
-            good_guesses.append(letter)
-        else:
-            print("Bad Guess!\n")
-            bad_guesses.append(letter)
-            guess_count += 1
-
-        if guess_count == guess_max:
-            print("\nYou Loose! The mystery word was {}".format(("".join(word))))
-            break
-        # else:
-        #     print("\nWIN!")
-        #     break
+game()
